df8481ebac31710a4ddef29ad47bef2e8a2cfaae
LAFATRA <106657164+laf551@users.noreply.github.com>
2024-01-11 17:05:27 +0000

    learning format

--- test-project/Begin.cs
+++ test-project/Begin.cs
@@ -1,20 +1,29 @@
-using System; 
 
-namespace ApplicationConsole {   /** Object name **/
-    class Program {
-        static void Main (string[] args){
-          Console.WriteLine("Hello World");
-            
-            Habit_Tracker heure = new Habit_Tracker(); 
-            Console.WriteLine(heure.msg) ;
-        }
-    }
-    class Habit_Tracker {
-        public string msg = "You slept at 7h"; 
-        
-        
-    }
-   
-}
 
+using System;
+namespace ApplicationConsole {
+  
+  class HelloWorld {
+    
+    static void Main () {
+       
+        Console.WriteLine ("Hello World");
+        Habit_Tracker hour = new Habit_Tracker ();
+        Person p = new Person ();
+        DateTime yesterday = DateTime.Today.AddDays(-1);
+        Console.WriteLine ("your fisrt name is {0} , and your last name is {1}. {2} {3 :d}"/*only day, :t only hour*/, p.firstName, p.lastName,hour.msg ,  yesterday );
+
+    }
+  }
+   class Habit_Tracker {
+        public string msg = "You slept at 9 a.m on";
+    
+  }
+  class Person
+  {
+    public string firstName = "Phillipe";
+    public string lastName = " Jean";
+    public int age = 70 ; 
+    
+  }
 }
