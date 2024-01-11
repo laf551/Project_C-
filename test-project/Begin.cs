f57a331f208f1e040bcc59673ba666fa1d01a3c3
LAFATRA <106657164+laf551@users.noreply.github.com>
2024-01-11 15:27:04 +0000

    new class

--- test-project/Begin.cs
+++ test-project/Begin.cs
@@ -3,7 +3,18 @@ using System;
 namespace ApplicationConsole {   /** Object name **/
     class Program {
         static void Main (string[] args){
-          Console.WriteLine("hello word!");
+          Console.WriteLine("Hello World");
+            
+            Habit_Tracker heure = new Habit_Tracker(); 
+            Console.WriteLine(heure.msg) ;
         }
     }
+    class Habit_Tracker {
+        public string msg = "You slept at 7h"; 
+        
+        
+    }
+   
+}
+
 }
